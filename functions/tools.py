24bdaf15f9c4526caa63847d58b492eb80479c24
Zeno <144168045+Zeno79@users.noreply.github.com>
2025-03-07 22:23:32 +0530

    Update tools.py

--- functions/tools.py
+++ functions/tools.py
@@ -1,21 +1,3 @@
-#    This file is part of the AutoAnime distribution.
-#    Copyright (c) 2025 Kaif_00z
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, version 3.
-#
-#    This program is distributed in the hope that it will be useful, but
-#    WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-#    General Public License for more details.
-#
-# License can be found in <
-# https://github.com/kaif-00z/AutoAnimeBot/blob/main/LICENSE > .
-
-# if you are using this following code then don't forgot to give proper
-# credit to t.me/kAiF_00z (github.com/kaif-00z)
-
 import asyncio
 import json
 import math
