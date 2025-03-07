ed92dcefef4dc45bec1a9d614af5726b49fa8775
Zeno <144168045+Zeno79@users.noreply.github.com>
2025-03-07 22:03:44 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,13 +13,11 @@
 # License can be found in <
 # https://github.com/kaif-00z/AutoAnimeBot/blob/main/LICENSE > .
 
-# if you are using this following code then don't forgot to give proper
+# If you are using this code, don't forget to give proper
 # credit to t.me/kAiF_00z (github.com/kaif-00z)
 
 from traceback import format_exc
-
 from telethon import Button, events
-
 from core.bot import Bot
 from core.executors import Executors
 from database import DataBase
@@ -40,7 +38,6 @@ torrent = Torrent()
 schedule = ScheduleTasks(bot)
 admin = AdminUtils(dB, bot)
 
-
 @bot.on(
     events.NewMessage(
         incoming=True, pattern="^/start ?(.*)", func=lambda e: e.is_private
@@ -50,13 +47,12 @@ async def _start(event):
     xnx = await event.reply("`Please Wait...`")
     msg_id = event.pattern_match.group(1)
     await dB.add_broadcast_user(event.sender_id)
+
     if Var.FORCESUB_CHANNEL and Var.FORCESUB_CHANNEL_LINK:
         is_user_joined = await bot.is_joined(Var.FORCESUB_CHANNEL, event.sender_id)
-        if is_user_joined:
-            pass
-        else:
+        if not is_user_joined:
             return await xnx.edit(
-                f"**Please Join The Following Channel To Use This Bot 🫡**",
+                "**Please Join The Following Channel To Use This Bot 🫡**",
                 buttons=[
                     [Button.url("🚀 JOIN CHANNEL", url=Var.FORCESUB_CHANNEL_LINK)],
                     [
@@ -67,6 +63,7 @@ async def _start(event):
                     ],
                 ],
             )
+
     if msg_id:
         if msg_id.isdigit():
             msg = await bot.get_messages(Var.BACKUP_CHANNEL, ids=int(msg_id))
@@ -84,7 +81,7 @@ async def _start(event):
                 buttons=admin.admin_panel(),
             )
         await event.reply(
-            f"**Enjoy Ongoing Anime's Best Encode 24/7 🫡**",
+            "**Enjoy Ongoing Anime's Best Encode 24/7 🫡**",
             buttons=[
                 [
                     Button.url("👨‍💻 DEV", url="t.me/kaif_00z"),
@@ -97,54 +94,44 @@ async def _start(event):
         )
     await xnx.delete()
 
-
-@bot.on(
-    events.NewMessage(incoming=True, pattern="^/about", func=lambda e: e.is_private)
-)
+@bot.on(events.NewMessage(incoming=True, pattern="^/about", func=lambda e: e.is_private))
 async def _(e):
     await admin._about(e)
-
 
 @bot.on(events.callbackquery.CallbackQuery(data="slog"))
 async def _(e):
     await admin._logs(e)
 
-
 @bot.on(events.callbackquery.CallbackQuery(data="sret"))
 async def _(e):
     await admin._restart(e, schedule)
-
 
 @bot.on(events.callbackquery.CallbackQuery(data="entg"))
 async def _(e):
     await admin._encode_t(e)
 
-
 @bot.on(events.callbackquery.CallbackQuery(data="butg"))
 async def _(e):
     await admin._btn_t(e)
-
 
 @bot.on(events.callbackquery.CallbackQuery(data="scul"))
 async def _(e):
     await admin._sep_c_t(e)
 
-
 @bot.on(events.callbackquery.CallbackQuery(data="cast"))
 async def _(e):
     await admin.broadcast_bt(e)
-
 
 @bot.on(events.callbackquery.CallbackQuery(data="bek"))
 async def _(e):
     await e.edit(buttons=admin.admin_panel())
 
-
 async def anime(data):
     try:
-        torr = [data.get("480p"), data.get("720p"), data.get("1080p")]
+        torr = [data.get("360p"), data.get("480p"), data.get("720p"), data.get("1080p")]
         anime_info = AnimeInfo(torr[0].title)
         poster = await tools._poster(bot, anime_info)
+
         if await dB.is_separate_channel_upload():
             chat_info = await tools.get_chat_info(bot, anime_info, dB)
             await poster.edit(
@@ -158,15 +145,21 @@ async def anime(data):
                 ]
             )
             poster = await tools._poster(bot, anime_info, chat_info["chat_id"])
+
         btn = [[]]
         original_upload = await dB.is_original_upload()
         button_upload = await dB.is_button_upload()
+
         for i in torr:
+            if not i:
+                continue  # Skip if no link for a resolution
+
             try:
                 filename = f"downloads/{i.title}"
                 reporter = Reporter(bot, i.title)
                 await reporter.alert_new_file_founded()
                 await torrent.download_magnet(i.link, "./downloads/")
+
                 exe = Executors(
                     bot,
                     dB,
@@ -179,6 +172,7 @@ async def anime(data):
                     reporter,
                 )
                 result, _btn = await exe.execute()
+
                 if result:
                     if _btn:
                         if len(btn[0]) == 2:
@@ -186,19 +180,23 @@ async def anime(data):
                         else:
                             btn[0].append(_btn)
                         await poster.edit(buttons=btn)
+
                     asyncio.ensure_future(exe.further_work())
                     continue
+
                 await reporter.report_error(_btn, log=True)
                 await reporter.msg.delete()
+
             except BaseException:
                 await reporter.report_error(str(format_exc()), log=True)
                 await reporter.msg.delete()
+                
     except BaseException:
         LOGS.error(str(format_exc()))
-
 
 try:
     bot.loop.run_until_complete(subsplease.on_new_anime(anime))
     bot.run()
 except KeyboardInterrupt:
     subsplease._exit()
+    
