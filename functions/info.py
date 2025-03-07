5ebdba19bf3cdc6d5b4383ee5628751e92b9058c
Zeno <144168045+Zeno79@users.noreply.github.com>
2025-03-07 21:02:41 +0530

    Update info.py

--- functions/info.py
+++ functions/info.py
@@ -1,3 +1,21 @@
+#    This file is part of the AutoAnime distribution.
+#    Copyright (c) 2025 Kaif_00z
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, version 3.
+#
+#    This program is distributed in the hope that it will be useful, but
+#    WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+#    General Public License for more details.
+#
+# License can be found in <
+# https://github.com/kaif-00z/AutoAnimeBot/blob/main/LICENSE > .
+
+# if you are using this following code then don't forgot to give proper
+# credit to t.me/kAiF_00z (github.com/kaif-00z)
+
 from traceback import format_exc
 
 import anitopy
@@ -9,15 +27,76 @@ from libs.logger import LOGS
 class AnimeInfo:
     def __init__(self, name):
         self.kitsu = RawAnimeInfo()
-        self.CAPTION = """× {} | {} ×
-╭──────────────────────── 
-│Season: {}
-│Episode: {}
-│Language: Japanese [ESub]
-│Quality: 360p, 480p, 720p, 1080p
-├────────────────────────
-│ @Anime_Wide | @Team_Wide
-╰────────────────────────
+        self.CAPTION = """
+**{}
+━━━━━━━━━━━━━━━
+‣ Language:** `Japanese [#    This file is part of the AutoAnime distribution.
+#    Copyright (c) 2025 Kaif_00z
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, version 3.
+#
+#    This program is distributed in the hope that it will be useful, but
+#    WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+#    General Public License for more details.
+#
+# License can be found in <
+# https://github.com/kaif-00z/AutoAnimeBot/blob/main/LICENSE > .
+
+# if you are using this following code then don't forgot to give proper
+# credit to t.me/kAiF_00z (github.com/kaif-00z)
+
+from decouple import config
+
+
+class Var:
+    # Version
+
+    __version__ = "v0.0.8"
+
+    # Telegram Credentials
+
+    API_ID = config("API_ID", default=6, cast=int)
+    API_HASH = config("API_HASH", default="eb06d4abfb49dc3eeb1aeb98ae0f581e")
+    BOT_TOKEN = config("BOT_TOKEN", default=None)
+    SESSION = config("SESSION", default=None)
+
+    # Database Credentials
+
+    MONGO_SRV = config("MONGO_SRV", default=None)
+
+    # Channels Ids
+
+    BACKUP_CHANNEL = config("BACKUP_CHANNEL", default=0, cast=int)
+    MAIN_CHANNEL = config("MAIN_CHANNEL", cast=int)
+    LOG_CHANNEL = config("LOG_CHANNEL", cast=int)
+    CLOUD_CHANNEL = config("CLOUD_CHANNEL", cast=int)
+    FORCESUB_CHANNEL = config("FORCESUB_CHANNEL", default=0, cast=int)
+    OWNER = config("OWNER", default=0, cast=int)
+
+    # Other Configs
+
+    THUMB = config(
+        "THUMBNAIL", default="https://graph.org/file/ad1b25807b81cdf1dff65.jpg"
+    )
+    FFMPEG = config("FFMPEG", default="ffmpeg")
+    CRF = config("CRF", default="27")
+    SEND_SCHEDULE = config("SEND_SCHEDULE", default=False, cast=bool)
+    RESTART_EVERDAY = config("RESTART_EVERDAY", default=True, cast=bool)
+    LOG_ON_MAIN = config("LOG_ON_MAIN", default=False, cast=bool)
+    FORCESUB_CHANNEL_LINK = config("FORCESUB_CHANNEL_LINK", default="", cast=str)
+
+    # Dev Configs
+
+    DEV_MODE = config("DEV_MODE", default=False, cast=bool)
+]`
+**‣ Quality:** `480p|720p|1080p`
+**‣ Season:** `{}`
+**‣ Episode:** `{}`
+**━━━━━━━━━━━━━━━**
+**‣Join :- @Unfold_Network
 """
         self.proper_name = self.get_proper_name_for_func(name)
         self.name = name
@@ -55,7 +134,6 @@ class AnimeInfo:
             if self.proper_name or self.data:
                 return self.CAPTION.format(
                     (await self.get_english()),
-                    self.proper_name,
                     str(self.data.get("anime_season") or 1).zfill(2),
                     (
                         str(self.data.get("episode_number")).zfill(2)
@@ -72,15 +150,17 @@ class AnimeInfo:
             anime_name = self.data.get("anime_title")
             if anime_name and self.data.get("episode_number"):
                 return (
-                    f"[S{self.data.get('anime_season') or 1}-{self.data.get('episode_number') or ''}] {(await self.get_english())} [{self.data.get('video_resolution')}].mkv"
-                    .replace("‘", "")
+                    f"[S{self.data.get('anime_season') or 1}-{self.data.get('episode_number') or ''}] {(await self.get_english())} [{self.data.get('video_resolution')}].mkv".replace(
+                        "‘", ""
+                    )
                     .replace("’", "")
                     .strip()
                 )
             if anime_name:
                 return (
-                    f"{(await self.get_english())} [{self.data.get('video_resolution')}].mkv"
-                    .replace("‘", "")
+                    f"{(await self.get_english())} [{self.data.get('video_resolution')}].mkv".replace(
+                        "‘", ""
+                    )
                     .replace("’", "")
                     .strip()
                 )
